aee361c4ba7bb5e42797c59b4f544399450ed00f
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-02-21 22:57:04 -0500

    conf(app): default imports config added

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
