19bdf200a4ed765e1aaddd03bad726fd98d4955e
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-02-22 07:31:58 -0500

    conf(web): home page story added

---
+++ apps/web/src/pages/home/home.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { HomePage } from "./home";
+
+const meta: Meta<typeof HomePage> = {
+  title: "Pages/HomePage",
+  component: HomePage,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof HomePage>;
+
+export const Default: Story = {};
