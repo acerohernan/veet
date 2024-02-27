4fd78d833950b08d5f9926328e482825b1ce7dbf
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-02-26 21:32:41 -0500

    test(web): redux added to storybook

--- apps/web/.storybook/preview.tsx
+++ apps/web/.storybook/preview.tsx
@@ -1,9 +1,13 @@
+import React from "react";
+import { Provider } from "react-redux";
 import { CssBaseline } from "@mui/material";
+import { MemoryRouter } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
 import type { Preview } from "@storybook/react";
 import { withThemeFromJSXProvider } from "@storybook/addon-themes";
 
 import { theme } from "../src/theme";
+import { store } from "../src/store";
 
 const preview: Preview = {
   parameters: {
@@ -26,6 +30,13 @@ export const decorators = [
     Provider: ThemeProvider,
     GlobalStyles: CssBaseline,
   }),
+  (Story) => (
+    <Provider store={store}>
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    </Provider>
+  ),
 ];
 
 export default preview;
