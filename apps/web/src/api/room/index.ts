e1c186cab019f49143168131e0d36148403932ca
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-02-27 17:33:04 -0500

    fix(web): include query at getting demo creds from endpoint

--- apps/web/src/api/room/index.ts
+++ apps/web/src/api/room/index.ts
@@ -22,7 +22,7 @@ export const getDemoCredentials = async (participant: Participant): Promise<APIR
   try {
     const query = new URLSearchParams();
     query.append("participantName", participant.name);
-    const res = await axiosInstance.get("/room/demo");
+    const res = await axiosInstance.get("/room/demo?" + query.toString());
     return {
       statusCode: res.status,
       data: res.data,
