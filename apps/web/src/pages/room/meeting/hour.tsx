054f3008146db42bb83619cd71e7a33b695e50a8
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-03-01 20:00:12 -0500

    feat(web): meeting hour component created

---
+++ apps/web/src/pages/room/meeting/hour.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { Typography } from "@mui/material";
+
+const getCurrentTime = () => new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+export const MeetingHour = () => {
+  const [currentTime, setCurrentTime] = useState(getCurrentTime);
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentTime(getCurrentTime), 20 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <Typography color="white" fontSize="1.1rem" fontWeight={300}>
+      {currentTime}
+    </Typography>
+  );
+};
